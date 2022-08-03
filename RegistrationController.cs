4dbd4aa3eaba0807f02d22bdca484ee0cf65e974
Sallal Haider <87648696+Sidhugee@users.noreply.github.com>
2022-08-03 15:53:22 +0500

    Add files via upload

---
+++ RegistrationController.cs
@@ -0,0 +1,56 @@
+﻿using OnlineShopping.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace OnlineShopping.Controllers
+{
+    [AllowAnonymous]
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Models.Membership model)
+        {
+            using(var Entities = new Entities())
+            {
+              bool IsValid =  Entities.Users.Any(x=>x.Username == model.Username && x.Password == model.Password);
+                if (IsValid)
+                {
+                    //Creates an authentication ticket of supplied username and adds it to the cookies collection of response
+                    FormsAuthentication.SetAuthCookie(model.Username, false);
+                    return RedirectToAction("Index", "Product");
+                }
+                ModelState.AddModelError("","Invalid Username And Password");
+                return View();
+            }
+        }
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(User model)
+        {
+            using (var Entities = new Entities())
+            {
+                Entities.Users.Add(model);
+                Entities.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+        public ActionResult Logout()
+        {
+            //Removes the form-authentication from the browser 
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
